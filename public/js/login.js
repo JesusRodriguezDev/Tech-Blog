f1920718851cd463d2b934b23d1c8e29c1f65d6a
Jesus Rodriguez <jesusrodriguez@Jesuss-MacBook-Pro.local>
2022-07-16 14:41:58 -0600

    fixed syntax error in js file

--- public/js/login.js
+++ public/js/login.js
@@ -1,31 +1,24 @@
 const loginFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const username = document.querySelector("#username-login").value.trim();
-    const password = document.querySelector("#password-login").value.trim();
-  
-    if (username && password) {
-      const response = await fetch("/api/users/login", {
-        method: "POST",
-        body: JSON.stringify({ username, password }),
-        headers: { "Content-Type": "application/json" },
-      });
-  
-      if (response.ok) {
-        document.location.replace("/dashboard");
-      } else {
-        alert(response.statusText);
-      }
+  event.preventDefault();
+
+  const username = document.querySelector("#username-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (username && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
     }
-  };
-  
-  document
-    .querySelector(".login-form")
-    .addEventListener("submit", loginFormHandler);
-  Footer
-  © 2022 GitHub, Inc.
-  Footer navigation
-  Terms
-  Privacy
-  Security
-  
+  }
+};
+
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
